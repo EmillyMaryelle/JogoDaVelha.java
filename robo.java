258c2c7b5e7ce37f764ae1edb29cffa5ef8f6ff1
Emilly Maryelle <emillymaryelle@gmail.com>
2022-01-12 15:57:21 -0300

    Create robo.java

---
+++ robo.java
@@ -0,0 +1,17 @@
+public class robo extends Jogador{
+    
+    public robo(int jogador2){
+        super(jogador2);
+        System.out.println("Jogador 'robo' criado!");
+    }
+    
+    
+    public void jogar(areaJogo areaJogo){
+        
+    }
+    
+    
+    public void Tentativa(areaJogo areaJogo){
+        
+    }
+}
